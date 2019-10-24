eb438a78bd2d773b8cf37a513d716a0c3a90bbd3
YehudaHermawan <yehudahermawan456@gmail.com>
2019-10-25 06:47:27 +0700

    25-10-19

--- app/src/main/java/com/example/recyclemodeviewgrid/MainActivity.java
+++ app/src/main/java/com/example/recyclemodeviewgrid/MainActivity.java
@@ -14,11 +14,13 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rvHeroes;
     private ArrayList<Hero> list = new ArrayList<>();
+    private String title = "Mode List";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setActionBarTitle(title);
 
         rvHeroes = findViewById(R.id.rv_heroes);
         rvHeroes.setHasFixedSize(true);
@@ -39,6 +41,18 @@ public class MainActivity extends AppCompatActivity {
         rvHeroes.setAdapter(gridHeroAdapter);
     }
 
+    private void showRecyclerCardView(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        CardViewHeroAdapter cardViewHeroAdapter = new CardViewHeroAdapter(list);
+        rvHeroes.setAdapter(cardViewHeroAdapter);
+    }
+
+    private void setActionBarTitle(String title) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -52,16 +66,20 @@ public class MainActivity extends AppCompatActivity {
     public void setMode(int selectedMode) {
         switch (selectedMode) {
             case R.id.action_list:
+                title = "Mode List";
                 showRecyclerList();
                 break;
 
             case R.id.action_grid:
+                title = "Mode Grid";
                 showRecyclerGrid();
                 break;
 
             case R.id.action_cardview:
-
+                title = "Mode CardView";
+                showRecyclerCardView();
                 break;
         }
+        setActionBarTitle(title);
     }
 }
